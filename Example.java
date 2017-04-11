97bec312e9c9082fb2ae78df3eba3de8b1678a34
erikmura44 <erikmura44@gmail.com>
2017-04-11 12:11:28 -0600

    initial

---
+++ Example.java
@@ -0,0 +1,31 @@
+public class Example {
+
+  public static void main(String[] args) {
+    // Your amazing code goes here...
+    System.out.println("We are making a new PEZ Dispenser");
+    System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n", PezDispenser.MAX_PEZ);
+    PezDispenser dispenser = new PezDispenser("Rafael");
+    System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+    if(dispenser.isEmpty()){
+      System.out.println("Dispenser is empty");
+    }
+    System.out.println("Filling the dispenser with delicious PEZ");
+    dispenser.fill();
+    if(!dispenser.isEmpty()){
+      System.out.println("Dispenser is full");
+    }
+    while(dispenser.dispense()){
+      System.out.println("Chomp!");
+    }
+    if(dispenser.isEmpty()){
+      System.out.println("Ate all the PEZ");
+    }
+    dispenser.fill(4);
+    dispenser.fill(2);
+    while(dispenser.dispense()) {
+      System.out.println("Chomp Chomp!!");
+    }
+
+  }
+
+}
---
+++ PezDispenser.java
@@ -0,0 +1,35 @@
+class PezDispenser {
+  public static final int MAX_PEZ = 12;
+  final private String characterName;
+  private int pezCount;
+
+  public PezDispenser(String characterName){
+    this.characterName = characterName;
+    pezCount = 0;
+  }
+
+  public void fill(){
+    fill(MAX_PEZ);
+  }
+
+  public void fill(int pezAmount){
+     pezCount += pezAmount;
+  }
+
+  public boolean dispense(){
+    boolean wasDispensed = false;
+    if(!isEmpty()){
+      pezCount--;
+      wasDispensed = true;
+    }
+    return wasDispensed;
+  }
+
+  public boolean isEmpty(){
+    return pezCount == 0;
+  }
+
+  public String getCharacterName(){
+    return characterName;
+  }
+}
